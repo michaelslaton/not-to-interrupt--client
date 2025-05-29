c859aa46260f2cc1e21812940e9bae8e8a2293a2
RenShadecrest <michaelaaronphoto@gmail.com>
2025-05-29 16:06:30 -0700

    created styling controllers not in control of the user. Added a button for hand raising.

--- src/RoomsDisplay/components/room/user-controller/UserController.tsx
+++ src/RoomsDisplay/components/room/user-controller/UserController.tsx
@@ -1,6 +1,9 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHandPaper } from '@fortawesome/fontawesome-free-solid';
 import type { UserType } from '../../../../types/User.type';
 import { useAppStateContext } from '../../../RoomsDisplay';
 import './userController.css';
+import type { IconProp } from '@fortawesome/fontawesome-svg-core';
 
 type UserControllerProps = {
   user: UserType;
@@ -8,6 +11,11 @@ type UserControllerProps = {
 
 const UserController = ({ user }: UserControllerProps) => {
  const { appState, setAppState } = useAppStateContext();
+
+ const miniCheck = ():boolean => {
+  if (user.id === appState.user?.id) return false;
+  else return true
+ };
 
  const handleAfk = ():void => {
   setAppState((prev)=> ({
@@ -20,21 +28,52 @@ const UserController = ({ user }: UserControllerProps) => {
       }
     }
   }));
-  console.log('AFK class applied:', user.controller.afk ? 'true' : 'false');
-  console.log('Appstate Level AFK class applied:', appState.user?.controller);
  };
 
+  const handleHandUp = ():void => {
+    setAppState((prev)=> ({
+      ...prev,
+      user: {
+        ...prev.user!,
+        controller: {
+          ...prev.user!.controller,
+          handUp: !prev.user!.controller.handUp
+        }
+      }
+    }));
+  };
+
   return (
-    <div className='controller'>
-      <div className='controller__title'>
+    <div className={`controller ${miniCheck() && 'mini'}`}>
+
+      <div className={`controller__title ${miniCheck() && 'mini'}`}>
         {user.name}
       </div>
-      <div className='controller__afk-window'>
-        <div className={`controller__afk ${user.controller.afk && 'true'}`}>AFK</div>
-        {user.id === appState.user?.id &&
-          <button onClick={handleAfk}>AFK</button>
-        }
+
+      <div className='controller__window'>
+        <div className={`controller__small-window afk ${user.controller.afk && 'true'} ${miniCheck() && 'mini'}`}>
+          AFK
+        </div>
+        <button
+          className={`controller__small-window-button ${miniCheck() && 'mini'}`}
+          onClick={handleAfk}
+        >
+          AFK
+        </button>
       </div>
+
+      <div className='controller__window'>
+        <div className={`controller__small-window icon hand ${user.controller.handUp && 'up'} ${miniCheck() && 'mini'}`}>
+          <FontAwesomeIcon icon={faHandPaper as IconProp} />
+        </div>
+        <button
+          className={`controller__small-window-button ${miniCheck() && 'mini'}`}
+          onClick={handleHandUp}
+        >
+          Hand Up
+        </button>
+      </div>
+
     </div>
   );
 };
