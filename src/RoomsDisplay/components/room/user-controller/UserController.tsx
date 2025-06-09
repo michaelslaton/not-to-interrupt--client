7ea78f797eddc8413901b8314bed25d2c9e0c05a
RenShadecrest <michaelaaronphoto@gmail.com>
2025-06-09 16:46:14 -0700

    adjusted styling

--- src/RoomsDisplay/components/room/user-controller/UserController.tsx
+++ src/RoomsDisplay/components/room/user-controller/UserController.tsx
@@ -104,14 +104,14 @@ const UserController = ({ user }: UserControllerProps) => {
       </div>
 
       <div className='controller__controls-wrapper'>
-        <div className='controller__window'>
+        <div className={`controller__window ${miniCheck() && 'mini'}`}>
           { user.id === appState.user?.id && <Toggle action={handleAfk} data={user.controller.afk}/> }
           <div className={`controller__small-window afk ${user.controller.afk && 'true'} ${miniCheck() && 'mini'}`}>
             AFK
           </div>
         </div>
 
-        <div className='controller__window'>
+        <div className={`controller__window ${miniCheck() && 'mini'}`}>
           { user.id === appState.user?.id && <Toggle action={handleHandUp} data={user.controller.handUp}/> }
           <div className={`controller__small-window icon hand ${user.controller.handUp && 'up'} ${miniCheck() && 'mini'}`}>
             <FontAwesomeIcon icon={faHandPaper as IconProp} />
