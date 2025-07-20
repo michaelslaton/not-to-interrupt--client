be1e1c859e33789fab060c459e915131fe24e916
RenShadecrest <michaelaaronphoto@gmail.com>
2025-07-19 21:58:20 -0700

    continued testing

--- src/RoomsDisplay/roomsDisplay.test.tsx
+++ src/RoomsDisplay/roomsDisplay.test.tsx
@@ -63,4 +63,57 @@ describe('RoomsDisplay', () => {
 
     expect(screen.getByPlaceholderText('Room Name')).toBeInTheDocument();
   });
+
+  it('Does not create a user and advance when no name is entered.', async () => {
+    const user = userEvent.setup();
+    render(<RoomsDisplay />);
+    const userInput = screen.getByTestId('formInput-input');
+    const button = screen.getByRole('button', { name: 'Create User' });
+    
+    await user.clear(userInput)
+    await user.click(button);
+    
+    const getRoomListHandlerCall = (mockSocket.on as ReturnType<typeof vi.fn>).mock.calls.find((call) => call[0] === 'getRoomList');
+    
+    if(getRoomListHandlerCall){
+      const getRoomListHandler = getRoomListHandlerCall[1];
+      act(() => {
+        getRoomListHandler([]);
+      });
+    };
+
+    const roomInput = screen.queryByPlaceholderText('Room Name');
+    expect(roomInput).not.toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+  });
+
+  it('Does not create a user and advance when entered name is too short.', async () => {
+    const user = userEvent.setup();
+    const getRoomListHandlerCall = (mockSocket.on as ReturnType<typeof vi.fn>).mock.calls.find((call) => call[0] === 'getRoomList');
+    render(<RoomsDisplay />);
+    const userInput = screen.getByTestId('formInput-input');
+    const button = screen.getByRole('button', { name: 'Create User' });
+    
+    await user.type(userInput, 'Re');
+    await user.click(button);
+    
+    if(getRoomListHandlerCall){
+      const getRoomListHandler = getRoomListHandlerCall[1];
+      act(() => {
+        getRoomListHandler([]);
+      });
+    };
+
+    let roomInput = screen.queryByPlaceholderText('Room Name');
+    expect(roomInput).not.toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+
+    await user.clear(userInput)
+    await user.type(userInput, 'R');
+    await user.click(button);
+    
+    roomInput = screen.queryByPlaceholderText('Room Name');
+    expect(roomInput).not.toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+  });
 });
